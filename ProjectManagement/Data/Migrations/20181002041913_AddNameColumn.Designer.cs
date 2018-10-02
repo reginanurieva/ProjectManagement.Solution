93724c9c22a6ffb56a93058a790038cfec3c4818
Hyewon Cho <jhng2525@gmail.com>
2018-10-01 21:24:09 -0700

    Add firstname, last name fields in registration.

--- ProjectManagement/Data/Migrations/20181002041913_AddNameColumn.Designer.cs
+++ ProjectManagement/Data/Migrations/20181002041913_AddNameColumn.Designer.cs
@@ -8,9 +8,10 @@ using ProjectManagement.Data;
 namespace ProjectManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181002041913_AddNameColumn")]
+    partial class AddNameColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
