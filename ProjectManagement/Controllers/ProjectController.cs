382977462e522714548561ed6c78d9799ac97bee
Hyewon Cho <jhng2525@gmail.com>
2018-10-02 23:28:35 -0700

    Add Authorization restriction on MyProjects page and get current user information.

--- ProjectManagement/Controllers/ProjectController.cs
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjectManagement.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProjectManagement.Controllers
 {
+    [Authorize]
     public class ProjectController : Controller
     {
         [HttpGet("/projects")]
