883f88b62ceeea2433f0a497519d261858fba7e7
Hyewon Cho <jhng2525@gmail.com>
2018-10-03 11:09:34 -0700

    Add my project detail page.

--- ProjectManagement/Controllers/ProjectController.cs
+++ ProjectManagement/Controllers/ProjectController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjectManagement.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjectManagement.Controllers
 {
@@ -25,6 +26,38 @@ namespace ProjectManagement.Controllers
             // Project foundProject = Project.Find(int.Parse(projectId));
             // foundProject.AddTodo(newTodo.Id);
             return RedirectToAction("Index");
+        }
+
+        [HttpGet("/projects/{projectId}")]
+        public ActionResult Details(int projectId)
+        {
+            Project foundProject = Project.Find(projectId);
+            return View(foundProject);
+        }
+
+        [HttpGet("/projects/update/{projectId}")]
+        public ActionResult Edit(int projectId)
+        {
+            Project foundProject = Project.Find(projectId);
+            return View(foundProject);
+        }
+
+        [HttpPost("/projects/update/{projectId}")]
+        public ActionResult EditProject(int projectId, string Name, string Content, DateTime DueDate, string Status, string returnUrl = null)
+        {
+            Project foundProject = Project.Find(projectId);
+            Project newProject = new Project(Name, Content, DueDate, Status);
+            foundProject.Update(newProject);
+            return RedirectToAction(nameof(ProjectController.Details), projectId);
+        }
+
+        [HttpPost("/projects/leave")]
+        public ActionResult LeaveProject(int projectId, int userId)
+        {
+            Project foundProject = Project.Find(projectId);
+            ProjectManagement.Models.User foundUser = ProjectManagement.Models.User.Find(userId);
+            foundUser.DeleteProject(foundProject);
+            return RedirectToAction(nameof(ProjectController.Index));
         }
     }
 }
